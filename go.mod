0a0e079fc4e8820c6d7af9fe6604418830f9bb06
Geoffrey White <geoffpiercewhite@gmail.com>
2025-11-18 14:24:19 -0500

    some changes

--- go.mod
+++ go.mod
@@ -2,15 +2,16 @@ module github.com/geofpwhite/snaketerm
 
 go 1.25.0
 
-require fortio.org/terminal v0.55.0
+require fortio.org/terminal v0.61.0
 
 require (
-	fortio.org/log v1.17.2 // indirect
+	fortio.org/log v1.18.3 // indirect
 	fortio.org/safecast v1.2.0 // indirect
 	fortio.org/struct2env v0.4.2 // indirect
-	github.com/kortschak/goroutine v1.1.2 // indirect
+	github.com/jbuchbinder/gopnm v0.0.0-20220507095634-e31f54490ce0 // indirect
+	github.com/kortschak/goroutine v1.1.3 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/image v0.31.0 // indirect
-	golang.org/x/sys v0.36.0 // indirect
-	golang.org/x/term v0.35.0 // indirect
+	golang.org/x/image v0.33.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/term v0.37.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"sync/atomic"
 
 	"fortio.org/terminal/ansipixels"
 	"fortio.org/terminal/ansipixels/tcolor"
@@ -18,47 +16,35 @@ func main() {
 	ap.HideCursor()
 	ap.ClearScreen()
 	s := snake.NewSnake(ap.W, ap.H)
-	var quit atomic.Bool
-	quit.Store(false)
-	go func() {
-		for {
-			curDir := atomic.LoadInt32(s.Dir)
-			if len(ap.Data) >= 3 {
-				switch ap.Data[2] {
-				case 65:
-					if curDir == snake.R || curDir == snake.L && s.CanChangeDir.Load() {
-						atomic.StoreInt32(s.Dir, snake.U)
-						s.CanChangeDir.Store(false)
-					}
-				case 66:
-					if curDir == snake.R || curDir == snake.L && s.CanChangeDir.Load() {
-						atomic.StoreInt32(s.Dir, snake.D)
-						s.CanChangeDir.Store(false)
-					}
-				case 67:
-					if curDir == snake.U || curDir == snake.D && s.CanChangeDir.Load() {
-						atomic.StoreInt32(s.Dir, snake.R)
-						s.CanChangeDir.Store(false)
-					}
-				case 68:
-					if curDir == snake.U || curDir == snake.D && s.CanChangeDir.Load() {
-						atomic.StoreInt32(s.Dir, snake.L)
-						s.CanChangeDir.Store(false)
-					}
+	ap.FPSTicks(func() bool {
+		ap.ClearScreen()
+		ap.WriteAt(0, 0, "%v", ap.Data)
+		if len(ap.Data) >= 3 {
+			switch ap.Data[2] {
+			case 65:
+				if s.Dir == snake.R || s.Dir == snake.L {
+					s.Dir = snake.U
+				}
+			case 66:
+				if s.Dir == snake.R || s.Dir == snake.L {
+					s.Dir = snake.D
+				}
+			case 67:
+				if s.Dir == snake.U || s.Dir == snake.D {
+					s.Dir = snake.R
+				}
+			case 68:
+				if s.Dir == snake.U || s.Dir == snake.D {
+					s.Dir = snake.L
 				}
 			}
-			if len(ap.Data) > 0 && ap.Data[0] == 'q' {
-				quit.Store(true)
-			}
 		}
-	}()
-
-	ap.FPSTicks(context.Background(), func(ctx context.Context) bool {
+		if len(ap.Data) > 0 && ap.Data[0] == 'q' {
+			return false
+		}
 		if !s.Next() {
 			return false
 		}
-		ap.ClearScreen()
-		ap.StartSyncMode()
 		mouthCoords := s.Snake[len(s.Snake)-1]
 		ap.WriteAt(mouthCoords.X, mouthCoords.Y, "%s ", ap.ColorOutput.Background(tcolor.Red.Color()))
 		foodCoords := s.Food
@@ -66,9 +52,8 @@ func main() {
 		for _, coords := range s.Snake[:len(s.Snake)-1] {
 			ap.WriteAt(coords.X, coords.Y, "%s ", ap.ColorOutput.Background(tcolor.White.Color()))
 		}
-		ap.WriteBg(tcolor.Black.Color())
+		ap.WriteString(tcolor.Reset)
 
-		ap.EndSyncMode()
-		return !quit.Load()
+		return true
 	})
 }
