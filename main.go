2e64024f3e687c32cdd700976b516adfe289a2de
Geoffrey White <geoffpiercewhite@gmail.com>
2025-11-19 11:51:12 -0500

    some cleanup + -square flag to draw half pixels

--- main.go
+++ main.go
@@ -5,14 +5,23 @@ import (
 
 	"fortio.org/terminal/ansipixels"
 	"fortio.org/terminal/ansipixels/tcolor"
-	"github.com/geofpwhite/snake-term-ansipixels/snake"
 )
 
 func main() {
 	fps := flag.Float64("fps", 60, "set fps")
+	halfFlag := flag.Bool("square", false, "use half height blocks so the snake's body is more square")
+
 	flag.Parse()
+	draw := drawFull
+	if *halfFlag {
+		draw = drawHalf
+	}
 	ap := ansipixels.NewAnsiPixels(*fps)
-	ap.Open()
+	err := ap.Open()
+	ap.SyncBackgroundColor()
+	if err != nil {
+		panic("error opening terminal")
+	}
 	defer func() {
 		ap.Restore()
 		ap.ShowCursor()
@@ -20,45 +29,128 @@ func main() {
 	}()
 	ap.HideCursor()
 	ap.ClearScreen()
-	s := snake.NewSnake(ap.W, ap.H)
-	ap.FPSTicks(func() bool {
-		ap.ClearScreen()
-		ap.WriteAt(0, 0, "%v", ap.Data)
-		if len(ap.Data) >= 3 {
-			switch ap.Data[2] {
-			case 65:
-				if s.Dir == snake.R || s.Dir == snake.L {
-					s.Dir = snake.U
-				}
-			case 66:
-				if s.Dir == snake.R || s.Dir == snake.L {
-					s.Dir = snake.D
-				}
-			case 67:
-				if s.Dir == snake.U || s.Dir == snake.D {
-					s.Dir = snake.R
-				}
-			case 68:
-				if s.Dir == snake.U || s.Dir == snake.D {
-					s.Dir = snake.L
-				}
-			}
-		}
+	h := ap.H
+	if *halfFlag {
+		h = ap.H * 2
+	}
+	s := newSnake(ap.W, h)
+	_ = ap.FPSTicks(func() bool {
 		if len(ap.Data) > 0 && ap.Data[0] == 'q' {
 			return false
 		}
-		if !s.Next() {
+		if len(ap.Data) >= 3 {
+			switch ap.Data[2] {
+			case 65:
+				if s.dir == R || s.dir == L {
+					s.dir = U
+				}
+			case 66:
+				if s.dir == R || s.dir == L {
+					s.dir = D
+				}
+			case 67:
+				if s.dir == U || s.dir == D {
+					s.dir = R
+				}
+			case 68:
+				if s.dir == U || s.dir == D {
+					s.dir = L
+				}
+			}
+		}
+		ap.ClearScreen()
+		if !s.next() {
 			return false
 		}
-		mouthCoords := s.Snake[len(s.Snake)-1]
-		ap.WriteAt(mouthCoords.X, mouthCoords.Y, "%s ", ap.ColorOutput.Background(tcolor.Red.Color()))
-		foodCoords := s.Food
-		ap.WriteAt(foodCoords.X, foodCoords.Y, "%s ", ap.ColorOutput.Background(tcolor.Green.Color()))
-		for _, coords := range s.Snake[:len(s.Snake)-1] {
-			ap.WriteAt(coords.X, coords.Y, "%s ", ap.ColorOutput.Background(tcolor.White.Color()))
-		}
-		ap.WriteString(tcolor.Reset)
-
+		draw(ap, s)
 		return true
 	})
+}
+
+func drawFull(ap *ansipixels.AnsiPixels, s *snake) {
+	mouthCoords := s.snake[len(s.snake)-1]
+	ap.WriteAt(mouthCoords.X, mouthCoords.Y, "%s ", ap.ColorOutput.Background(tcolor.Red.Color()))
+	foodCoords := s.food
+	ap.WriteAt(foodCoords.X, foodCoords.Y, "%s ", ap.ColorOutput.Background(tcolor.Green.Color()))
+	for _, coords := range s.snake[:len(s.snake)-1] {
+		ap.WriteAt(coords.X, coords.Y, "%s ", ap.ColorOutput.Background(tcolor.White.Color()))
+	}
+	ap.WriteString(tcolor.Reset)
+}
+
+type pixel struct {
+	top, bottom           bool
+	topColor, bottomColor tcolor.Color
+}
+
+func drawHalf(ap *ansipixels.AnsiPixels, s *snake) {
+	pix := make(map[coords]*pixel)
+	color := tcolor.White.Color()
+	l := len(s.snake)
+	for i, coords := range s.snake {
+		if i == l-1 {
+			color = tcolor.Red.Color()
+		}
+		if coords.Y%2 == 0 {
+			coords.Y /= 2
+			if pix[coords] == nil {
+				pix[coords] = &pixel{}
+			}
+			pix[coords].top = true
+			pix[coords].topColor = color
+		} else {
+			coords.Y /= 2
+			if pix[coords] == nil {
+				pix[coords] = &pixel{}
+			}
+			pix[coords].bottom = true
+			pix[coords].bottomColor = color
+		}
+	}
+	fy := coords{s.food.X, s.food.Y / 2}
+	if pix[fy] == nil {
+		pix[fy] = &pixel{}
+	}
+	if s.food.Y%2 == 0 {
+		pix[fy].top = true
+		pix[fy].topColor = tcolor.Green.Color()
+	} else {
+		pix[fy].bottom = true
+		pix[fy].bottomColor = tcolor.Green.Color()
+	}
+	drawPixels(ap, pix)
+	ap.WriteString(tcolor.Reset)
+}
+
+func drawPixels(ap *ansipixels.AnsiPixels, pix map[coords]*pixel) {
+	var char rune
+	var bg, fg tcolor.Color
+	for coords, pixel := range pix {
+		switch {
+		case pixel.top && pixel.bottom:
+			if pixel.topColor == pixel.bottomColor {
+				char = ' '
+				bg = pixel.topColor
+				fg = pixel.topColor
+			} else {
+				char = ansipixels.BottomHalfPixel
+				bg = pixel.topColor
+				fg = pixel.bottomColor
+			}
+		case pixel.top:
+			char = ansipixels.TopHalfPixel
+			bg = ap.Background.Color()
+			fg = pixel.topColor
+		case pixel.bottom:
+			char = ansipixels.BottomHalfPixel
+			bg = ap.Background.Color()
+			fg = pixel.bottomColor
+		default:
+			continue
+		}
+		ap.MoveCursor(coords.X, coords.Y)
+		ap.WriteBg(bg)
+		ap.WriteFg(fg)
+		ap.WriteRune(char)
+	}
 }
---
+++ snake.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"math/rand/v2"
+)
+
+type coords struct{ X, Y int }
+
+type direction = int32
+
+const (
+	U direction = iota
+	D
+	L
+	R
+)
+
+var directionCoords = [4]coords{
+	{0, -1},
+	{0, 1},
+	{-1, 0},
+	{1, 0},
+}
+
+type snake struct {
+	m          map[coords]bool
+	food       coords
+	snake      []coords // last element is snake mouth
+	maxX, maxY int
+	dir        direction
+}
+
+func newSnake(mx, my int) *snake {
+	snak := make([]coords, 0, mx*my)
+	snak = append(snak, coords{mx / 2, my / 2})
+	m := make(map[coords]bool)
+	m[coords{mx / 2, my / 2}] = true
+	d := U
+	s := snake{snake: snak, food: coords{rand.IntN(mx), rand.IntN(my)}, maxX: mx, maxY: my, dir: d, m: m}
+	return &s
+}
+
+func (s *snake) next() bool {
+	length := len(s.snake)
+	dir := s.dir
+	changed := directionCoords[dir]
+	next := coords{(s.snake[length-1].X + changed.X) % s.maxX, (s.snake[length-1].Y + changed.Y) % s.maxY}
+	if next.X < 0 {
+		next.X += s.maxX
+	}
+	if next.Y < 0 {
+		next.Y += s.maxY
+	}
+	s.snake = append(s.snake, next)
+	s.m[next] = true
+	if s.food == next {
+		s.food = coords{rand.IntN(s.maxX), rand.IntN(s.maxY)}
+		return len(s.snake) == len(s.m)
+	}
+	delete(s.m, s.snake[0])
+	s.snake = s.snake[1:]
+	return len(s.snake) == len(s.m)
+}
